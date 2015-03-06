7fd40606cc92c8681dbc40cfbfa1251233f3d53a
Gulshan Singh <gulshan@umich.edu>
2015-03-05 19:11:43 -0500

    Implement basic arithmetic

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-#![feature(plugin)]
+#![feature(plugin, core)]
 #![plugin(peg_syntax_ext)]
+
+use lisp::expression;
 
 peg_file! lisp("grammar.rustpeg");
 
 fn main() {
+    assert_eq!(expression("1"), Ok(1));
+    assert_eq!(expression("(    +   1    1   )"), Ok(2));
+    assert_eq!(expression("(+ 1 1)"), Ok(2));
+    assert_eq!(expression("(- 6 2)"), Ok(4));
+    assert_eq!(expression("(* 3 4)"), Ok(12));
+    assert_eq!(expression("(/ 5 2)"), Ok(2));
+    assert_eq!(expression("(/ (+ 1 1) 2)"), Ok(1));
+    assert_eq!(expression("(/ (+ (- 4 1) 1) 2)"), Ok(2));
+    assert_eq!(expression("(* (+ 1 1) (+ 2 2))"), Ok(8));
     println!("Ok");
 }
